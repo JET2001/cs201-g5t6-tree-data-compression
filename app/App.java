a118fb758ced3728bc9aa77fe33686c071d6b292
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-10-26 02:23:33 +0800

    octree quantization and uniform quantization

--- app/App.java
+++ app/App.java
@@ -17,7 +17,7 @@ public class App {
         Utility Utility = new Utility();
 
         //Define original file directory to loop through
-        String ImageDirectory = "../Original/";
+        String ImageDirectory = "./Original/";
 
         // List all files in the directory
         File directory = new File(ImageDirectory);
@@ -49,7 +49,7 @@ public class App {
                     // Now you have the image data in 'pixelData' that will be taken in by Compress
 
                     // Define location and name for the compressed file to be created
-                    String compressed_file_name = "../Compressed/" + imageName.substring(0, imageName.lastIndexOf('.')) + ".bin";
+                    String compressed_file_name = "./Compressed/" + imageName.substring(0, imageName.lastIndexOf('.')) + ".bin";
 
                     // start compress timer
                     long compressStartTime = System.currentTimeMillis();
@@ -90,7 +90,7 @@ public class App {
 
                     //convert back to image for visualisation
                     PixeltoImageConverter PixeltoImageConverter = new PixeltoImageConverter(newPixelData);
-                    PixeltoImageConverter.saveImage("../Decompressed/" + imageName, "png");
+                    PixeltoImageConverter.saveImage("./Decompressed/" + imageName, "png");
 
                     //Get the two bufferedimages for calculations
                     BufferedImage originalimage = ImageIO.read(new File(ImageDirectory + imageName));
--- utility_basic/Utility.java
+++ utility_basic/Utility.java
@@ -20,32 +20,12 @@ public class Utility {
             return color;
         }
 
-        public void setColor(int color) {
-            this.color = color;
-        }
-
-        public int getFrequency() {
-            return frequency;
-        }
-
-        public void setFrequency(int frequency) {
-            this.frequency = frequency;
-        }
-
         public HuffmanNode getLeft() {
             return left;
         }
 
-        public void setLeft(HuffmanNode left) {
-            this.left = left;
-        }
-
         public HuffmanNode getRight() {
             return right;
-        }
-
-        public void setRight(HuffmanNode right) {
-            this.right = right;
         }
 
         boolean hasLeaf() {
@@ -80,10 +60,6 @@ public class Utility {
 
         public HuffmanNode getRoot() {
             return root;
-        }
-
-        public void setRoot(HuffmanNode root) {
-            this.root = root;
         }
 
         private HuffmanNode buildHuffmanTree(int[][][] imagePixels, Map<Integer, Integer> colorFrequencies) {
@@ -129,39 +105,177 @@ public class Utility {
         }
     }
 
-    final int numberOfColors = 52; // range from 2 - 256 (52 is the sweet spot)
+    class OctreeNode {
+        int pixelCount;
+        int red;
+        int green;
+        int blue;
+        OctreeNode[] children;
+    }
 
-    public int[][][] quantize(int[][][] imagePixels, int numberOfColors) {
-        int binSize = (int)Math.ceil(256 / numberOfColors);
-        for (int x = 0; x < imagePixels.length; x++) {
-            for (int y = 0; y < imagePixels[0].length; y++) {
-                for (int z = 0; z < imagePixels[0][0].length; z++) {
-                    int color = imagePixels[x][y][z];
-                    int binIndex = color / binSize;
-                    int quantizedColor = binSize * binIndex + binSize / 2;
-                    imagePixels[x][y][z] = quantizedColor;
+    class Octree {
+        OctreeNode root;
+        int numNodes;
+        int maxNodes;
+        List<List<OctreeNode>> levelBuckets;
+
+        public Octree(int maxNodes) {
+            root = new OctreeNode();
+            this.maxNodes = maxNodes;
+            levelBuckets = new ArrayList<>();
+            for (int i = 0; i <= 8; i++) {
+                levelBuckets.add(new ArrayList<>());
+            }
+        }
+
+        public void addColor(int red, int green, int blue) {
+            OctreeNode node = root;
+            for (int level = 1; level <= 8; level++) {
+                node.pixelCount++;
+                node.red += red;
+                node.green += green;
+                node.blue += blue;
+                int shift = 8 - level;
+                int index = ((red & (0x1 << shift)) > 0 ? 4 : 0) |
+                        ((green & (0x1 << shift)) > 0 ? 2 : 0) |
+                        ((blue & (0x1 << shift)) > 0 ? 1 : 0);
+                if (node.children == null) {
+                    node.children = new OctreeNode[8];
+                }
+                if (node.children[index] == null) {
+                    node.children[index] = new OctreeNode();
+                    numNodes++;
+                    levelBuckets.get(level).add(node.children[index]);
+                }
+                node = node.children[index];
+            }
+            node.pixelCount++;
+            node.red += red;
+            node.green += green;
+            node.blue += blue;
+        }
+
+        public void reduceTree() {
+            for (int level = 8; level >= 0; level--) {
+                if (levelBuckets.get(level).size() > 0) {
+                    OctreeNode node = levelBuckets.get(level).remove(0);
+                    node.pixelCount /= 8;
+                    node.red /= 8;
+                    node.green /= 8;
+                    node.blue /= 8;
+                    node.children = null;
+                    numNodes -= 7;
+                    if (numNodes <= maxNodes) {
+                        return;
+                    }
                 }
             }
         }
+
+        public int[] findColor(int red, int green, int blue) {
+            OctreeNode node = root;
+            for (int level = 1; level <= 8; level++) {
+                if (node.children == null) {
+                    break;
+                }
+                int shift = 8 - level;
+                int index = ((red & (0x1 << shift)) > 0 ? 4 : 0) |
+                        ((green & (0x1 << shift)) > 0 ? 2 : 0) |
+                        ((blue & (0x1 << shift)) > 0 ? 1 : 0);
+                node = node.children[index];
+            }
+            return new int[] { node.red / node.pixelCount, node.green / node.pixelCount, node.blue / node.pixelCount };
+        }
+    }
+
+    final int numberOfColors = 52; // range from 2 - 256 (24 is the sweet spot)
+
+    public int[][][] quantization(int[][][] imagePixels, int numberOfColors) {
+        // Create an Octree and add all colors from the image
+        Octree octree = new Octree(numberOfColors);
+        for (int[][] row : imagePixels) {
+            for (int[] pixel : row) {
+                octree.addColor(pixel[0], pixel[1], pixel[2]);
+            }
+        }
+
+        // Reduce the Octree to the desired number of colors
+        octree.reduceTree();
+
+        // Replace each pixel's color with the color of the Octree node that it belongs
+        // to
+        for (int x = 0; x < imagePixels.length; x++) {
+            for (int y = 0; y < imagePixels[0].length; y++) {
+                int[] color = octree.findColor(imagePixels[x][y][0], imagePixels[x][y][1],
+                        imagePixels[x][y][2]);
+                imagePixels[x][y]= color;
+            }
+        }
+
         return imagePixels;
     }
 
+    public int[][][] dither(int[][][] imagePixels) {
+        int[][][] ditheredImagePixels = new int[imagePixels.length][imagePixels[0].length][imagePixels[0][0].length];
+
+        for (int y = 0; y < imagePixels[0].length; y++) {
+            for (int x = 0; x < imagePixels.length; x++) {
+                for (int z = 0; z < imagePixels[0][0].length; z++) {
+                    int oldPixel = imagePixels[x][y][z];
+                    int newPixel = oldPixel < 128 ? 0 : 255;
+                    ditheredImagePixels[x][y][z] = newPixel;
+                    int quantError = oldPixel - newPixel;
+
+                    if (x + 1 < imagePixels.length) {
+                        imagePixels[x + 1][y][z] += quantError * 7 / 16;
+                    }
+                    if (y + 1 < imagePixels[0].length) {
+                        if (x - 1 >= 0) {
+                            imagePixels[x - 1][y + 1][z] += quantError * 3 / 16;
+                        }
+                        imagePixels[x][y + 1][z] += quantError * 5 / 16;
+                        if (x + 1 < imagePixels.length) {
+                            imagePixels[x + 1][y + 1][z] += quantError * 1 / 16;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ditheredImagePixels;
+    }
+
+    // public int[][][] quantization(int[][][] imagePixels, int numberOfColors) {
+    // int binSize = (int) Math.ceil(256 / numberOfColors);
+    // for (int x = 0; x < imagePixels.length; x++) {
+    // for (int y = 0; y < imagePixels[0].length; y++) {
+    // for (int z = 0; z < imagePixels[0][0].length; z++) {
+    // int color = imagePixels[x][y][z];
+    // int binIndex = color / binSize;
+    // int quantizedColor = binSize * binIndex + binSize / 2;
+    // imagePixels[x][y][z] = quantizedColor;
+    // }
+    // }
+    // }
+    // return imagePixels;
+    // }
+
     // Method to compress image data and save it to a file
     public void Compress(final int[][][] imagePixels, final String outputFileName) throws IOException {
-        //Quantize the image data
-        int[][][] quantizedImagePixels = quantize(imagePixels, numberOfColors);
+        // Quantize the image data
+        int[][][] quantizedImagePixels = quantization(imagePixels, numberOfColors);
 
         // Calculate color frequencies in the image
         Map<Integer, Integer> colorFrequencies = calculateColorFrequencies(quantizedImagePixels);
 
         // Build Huffman tree based on color frequencies
-        HuffmanTree huffmanTree = buildHuffmanTree(imagePixels, colorFrequencies);
+        HuffmanTree huffmanTree = buildHuffmanTree(quantizedImagePixels, colorFrequencies);
 
         // Generate Huffman codes for each color value
         Map<Integer, String> huffmanCodes = huffmanTree.generateHuffmanCodes();
 
         // Encode the image data using Huffman codes
-        List<String> encodedImageData = encodeImageData(imagePixels, huffmanCodes);
+        List<String> encodedImageData = encodeImageData(quantizedImagePixels, huffmanCodes);
 
         // Build the compressed data string
         String compressedDataString = buildCompressedDataString(encodedImageData);
@@ -188,7 +302,11 @@ public class Utility {
             byte[] compressedDataByteArray = (byte[]) ois.readObject();
 
             // Reconstruct the original image pixels
-            return reconstructImagePixels(huffmanTree, compressedDataByteArray);
+            int[][][] quantizedImagePixels = reconstructImagePixels(huffmanTree, compressedDataByteArray);
+
+            // Dequantize the image pixels
+            return quantization(quantizedImagePixels, numberOfColors);
+            // return quantizedImagePixels;
         }
     }
 
@@ -272,9 +390,9 @@ public class Utility {
                         }
                         int bit = (currentByte >> (7 - (currentBit % 8))) & 1;
                         currentBit++;
-        
+
                         currentNode = (bit == 0) ? currentNode.getLeft() : currentNode.getRight();
-        
+
                         if (currentNode.hasLeaf()) {
                             imagePixels[x][y][z] = currentNode.getColor();
                             break;
