97df8ef91907a8aacdb7e320f7fb6dbda35b63f2
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-10-26 03:06:31 +0800

    double quantisation

--- utility_basic/Utility.java
+++ utility_basic/Utility.java
@@ -190,7 +190,7 @@ public class Utility {
 
     final int numberOfColors = 52; // range from 2 - 256 (24 is the sweet spot)
 
-    public int[][][] quantization(int[][][] imagePixels, int numberOfColors) {
+    public int[][][] octreeQuantization(int[][][] imagePixels, int numberOfColors) {
         // Create an Octree and add all colors from the image
         Octree octree = new Octree(numberOfColors);
         for (int[][] row : imagePixels) {
@@ -245,25 +245,26 @@ public class Utility {
         return ditheredImagePixels;
     }
 
-    // public int[][][] quantization(int[][][] imagePixels, int numberOfColors) {
-    // int binSize = (int) Math.ceil(256 / numberOfColors);
-    // for (int x = 0; x < imagePixels.length; x++) {
-    // for (int y = 0; y < imagePixels[0].length; y++) {
-    // for (int z = 0; z < imagePixels[0][0].length; z++) {
-    // int color = imagePixels[x][y][z];
-    // int binIndex = color / binSize;
-    // int quantizedColor = binSize * binIndex + binSize / 2;
-    // imagePixels[x][y][z] = quantizedColor;
-    // }
-    // }
-    // }
-    // return imagePixels;
-    // }
+    public int[][][] quantization(int[][][] imagePixels, int numberOfColors) {
+    int binSize = (int) Math.ceil(256 / numberOfColors);
+    for (int x = 0; x < imagePixels.length; x++) {
+    for (int y = 0; y < imagePixels[0].length; y++) {
+    for (int z = 0; z < imagePixels[0][0].length; z++) {
+    int color = imagePixels[x][y][z];
+    int binIndex = color / binSize;
+    int quantizedColor = binSize * binIndex + binSize / 2;
+    imagePixels[x][y][z] = quantizedColor;
+    }
+    }
+    }
+    return imagePixels;
+    }
 
     // Method to compress image data and save it to a file
     public void Compress(final int[][][] imagePixels, final String outputFileName) throws IOException {
         // Quantize the image data
-        int[][][] quantizedImagePixels = quantization(imagePixels, numberOfColors);
+        int[][][] quantizedImagePixels = octreeQuantization(imagePixels, numberOfColors);
+        quantizedImagePixels = quantization(quantizedImagePixels, numberOfColors);
 
         // Calculate color frequencies in the image
         Map<Integer, Integer> colorFrequencies = calculateColorFrequencies(quantizedImagePixels);
